624dac6e1d4e0726c414722339001a34b77c7632
Amjad Hossain <amjad.jibon@gmail.com>
2026-08-31 21:30:47 +0800

    chore: init tokenbar xcode project

---
+++ TokenBarTests/TokenBarTests.swift
@@ -0,0 +1,19 @@
+//
+//  TokenBarTests.swift
+//  TokenBarTests
+//
+//  Created by Amjad Hossain on 31/08/2026.
+//
+
+import Testing
+@testable import TokenBar
+
+struct TokenBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
